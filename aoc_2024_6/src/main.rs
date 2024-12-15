7d6d5dda74f58eae2c921d0ca0d4cdf9105265ac
Jake Nelson <jake.nelzon@gmail.com>
2024-12-15 12:36:35 -0800

    day 6 part 2

--- aoc_2024_6/src/main.rs
+++ aoc_2024_6/src/main.rs
@@ -1,30 +1,78 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
-fn part_one_mouse(mut puzzle: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn part_one_mouse(mut puzzle: Vec<Vec<char>>, mut positions: HashSet<(usize, usize, usize)>) -> (Vec<Vec<char>>, HashSet<(usize,usize,usize)>, bool) {
     let mut dir:i32 = 0; // (0 ,1 , 2, 3)  % 4 // 0 = up, 1 = r, 2 = d, 3 = l
     let mut x:usize;
     let mut y:usize;
     (x,y) = find_start(&puzzle);
     loop {
+        // println!("{}", "");
+        // for row in &puzzle{
+        //     println!("{}", row.iter().collect::<String>());
+        // }
+        if positions.contains(&(x,y,dir as usize)) {
+            return (puzzle, positions, true);
+        }
+        positions.insert((x,y,dir as usize));
         //cover our current tile with an X
         puzzle[y][x] = 'X';
-        //take a step, else turn right
+        match dir  {
+            0 => {
+                puzzle[y][x] = '|';
+            }
+            1 => {
+                puzzle[y][x] = '-';
+            }
+            2 => {
+                puzzle[y][x] = '|';
+            }
+            3 => {
+                puzzle[y][x] = '-';
+            }
+            _ => {
+                puzzle[y][x] = 'X';
+            }
+        }
+        // take a step, else turn right
         let temp_x:i32;
         let temp_y:i32;
         (temp_x, temp_y, dir) = get_next_space(&puzzle, x, y, dir);
         if temp_x < 0 || temp_x >= puzzle[0].len() as i32 || temp_y < 0 || temp_y >= puzzle.len() as i32{
-            return puzzle;
+            return  (puzzle, positions, false);
         }
         x = temp_x as usize;
         y = temp_y as usize; 
-        // println!("{}", "- - - - - -");
-        // for row in &puzzle{
-        //     println!("{}", row.iter().collect::<String>());
-        // }
-
     }
+}
+
+fn part_two_mouse(puzzle: &Vec<Vec<char>>, mut positions: HashSet<(usize,usize,usize)>) -> usize {
+    let start = find_start(puzzle);
+    positions.remove(&(start.0,start.1,0));
+    let mut count:usize = 0;
+    let mut tested_positions:HashSet<(usize,usize)> = HashSet::new();
+    for (trial_x, trial_y, _trial_dir) in positions {
+        if tested_positions.contains(&(trial_x, trial_y)) {
+            continue;
+        }
+        let mut puzzle_copy = puzzle.clone();
+        puzzle_copy[trial_y][trial_x] = '#';
+        let detect_loop:bool;
+        let mut _positions :HashSet<(usize, usize, usize)> = HashSet::new();
+        (puzzle_copy, _positions, detect_loop) = part_one_mouse(puzzle_copy, _positions);
+        if detect_loop {
+            count += 1;
+            // println!("{}", "");
+            // println!("Solution {}", count);
+            // for row in &puzzle_copy{
+            //     println!("{}", row.iter().collect::<String>());
+            //  }
+        }
+        tested_positions.insert((trial_x, trial_y));
+    }
+    return count;
 }
 
 fn get_next_space(puzzle: &Vec<Vec<char>>, x:usize, y:usize, mut dir:i32) -> (i32,i32,i32) {
@@ -81,7 +129,6 @@ fn find_start(puzzle: &Vec<Vec<char>>) -> (usize, usize){
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
     const DEBUG:bool = false;
     let file_path = if DEBUG {"debug.txt"} else {"input.txt"};
     let file = File::open(file_path)?;
@@ -91,14 +138,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         let result = line?;
          puzzle.push(result.chars().collect());
     }
+    let mut positions:HashSet<(usize,usize,usize)> = HashSet::new();
+    let clean_puzzle = puzzle.clone();
+    (puzzle, positions, _) = part_one_mouse(puzzle, positions);
 
-    puzzle = part_one_mouse(puzzle);
-
+    let count = part_two_mouse(&clean_puzzle, positions);
     println!("There are {} tiles covered in part one.", puzzle.iter().map(|x| x.iter().filter(|y| **y == 'X').count() ).sum::<usize>() );
     println!("{}", "- - - - - -");
-    for row in &puzzle{
-        println!("{}", row.iter().collect::<String>());
-    }
+    println!("There are {} possible positions to loop the guard.", count);
+
+    // for row in &puzzle{
+    //     println!("{}", row.iter().collect::<String>());
+    // }
 
     Ok(())
 }
