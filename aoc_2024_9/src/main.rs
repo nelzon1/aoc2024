8fc97bb0606508314ab402414ccd33137d2a17ba
Jake Nelson <jake.nelzon@gmail.com>
2024-12-16 00:42:28 -0800

    day 9 pt 2 bugs

---
+++ aoc_2024_9/src/main.rs
@@ -0,0 +1,130 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::io::stdout;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    const DEBUG:bool = true;
+    let file_path = if DEBUG {"debug.txt"} else {"input.txt"};
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+    let mut puzzle:String = String::new();
+    for line in reader.lines() {
+        puzzle = line?;
+    }
+    //println!("{}", puzzle);
+    let mut disk_vec:Vec<u32> = Vec::new();
+    let mut disk_pages:Vec<(u32,u32)> = Vec::new();
+    // iterate over the puzzle and build the disk vector
+    for (i, a) in puzzle.chars().into_iter().enumerate() {
+        let file_len = a.to_digit(10).unwrap() as usize;
+        if i % 2 == 0 {
+            for _ in 0..file_len {
+                disk_vec.push((i / 2 ) as u32);
+                
+            }
+            disk_pages.push(((i/2) as u32, file_len as u32));
+        }
+        else {
+            for _ in 0..file_len {
+                disk_vec.push(999999999);
+            }
+            disk_pages.push((999999999, file_len as u32));
+        }
+    }
+    // literate until the disk string is compacted
+    while let Some(next_gap) = disk_vec.iter().position(|x| *x == 999999999) {
+        let last_block = &disk_vec.pop().unwrap();
+        disk_vec[next_gap] = *last_block;
+    }
+    // Part 2
+    let mut index = disk_pages.len()-1;
+    // literate until the disk string is compacted
+    while index > 0 {
+        let mut next_file = disk_pages.iter().nth(index).unwrap();
+        if let Some(next_block_to_move) = is_space_available(&disk_pages, next_file.1, index - 1) {
+            next_file = disk_pages.iter().nth(index).unwrap();
+            if next_file.0 == 999999999 {
+                index -= 1;
+                continue;
+            }
+            disk_pages = compress(*next_file, next_block_to_move, disk_pages);
+            print_disk(&disk_pages);
+        }
+        else {
+            index -= 1;
+        }
+    }
+    // get checksum
+    println!("The checksum is {}", checksum(disk_vec));
+    println!("The checksum is {}", checksum2(disk_pages));
+    Ok(())
+}
+
+fn is_space_available(disk_pages:&Vec<(u32,u32)>, file_size:u32, index:usize) -> Option<usize> {
+    return disk_pages.iter().take(index).position(|x| x.1 >= file_size && x.0 == 999999999);
+}
+
+fn compress(file:(u32,u32), index:usize, mut disk_pages:Vec<(u32,u32)>) -> Vec<(u32,u32)> {
+    if file.1 < disk_pages[index].1 {
+        disk_pages[index].1 -= file.1;
+        let mut slice = Vec::new();
+        slice.push(file);
+        //disk_pages.remove(disk_pages.iter().position(|x| *x == file).unwrap());
+        let old_position = disk_pages.iter().position(|x: &(u32, u32)| *x == file).unwrap();
+        disk_pages[old_position].0 = 999999999;
+        disk_pages.splice(index..index, slice);
+        return disk_pages;
+    }
+    else{
+        //disk_pages.remove(disk_pages.iter().position(|x| *x == file).unwrap());
+        let old_position = disk_pages.iter().position(|x: &(u32, u32)| *x == file).unwrap();
+        disk_pages[old_position].0 = 999999999;
+        disk_pages[index] = file;
+        return disk_pages;
+    }
+
+}
+
+fn checksum(input: Vec<u32>) -> u64 {
+    return input.iter()
+                .enumerate()
+                .map(|(i, x)| (i as u64) * *x as u64)
+                .sum();
+}
+
+fn checksum2(input: Vec<(u32,u32)>) -> u64 {
+    let mut count: u64 = 0;
+    let mut total: u64 = 0;
+    for page in input.iter(){
+        if page.0 != 999999999{
+            for _ in 0..page.1 {
+                total += count * page.0 as u64;
+                count += 1;
+            }
+    
+        } 
+        else{
+            count += page.1 as u64;
+        }
+    }
+    return total;
+}
+
+fn print_disk(input:& Vec<(u32,u32)>) {
+    println!();
+    for page in input{
+        for _ in 0..page.1 {
+            if page.0 == 999999999 {
+                print!("{}",".");
+                _ = stdout().flush();
+            }
+            else{
+                print!("{}",page.0);
+                _ =stdout().flush();
+            }
+        }
+    }
+    println!();
+}
